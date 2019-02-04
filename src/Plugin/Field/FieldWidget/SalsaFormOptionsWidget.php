fb4ae74d7c49393cd42cbf2e2a82e109e4a1dfb9
5adiyah <sadiali@pdx.edu>
2019-02-04 12:42:36 -0800

    Add widget to display options for Salsa Embed Form Type

---
+++ src/Plugin/Field/FieldWidget/SalsaFormOptionsWidget.php
@@ -0,0 +1,38 @@
+<?php
+namespace Drupal\bene_salsa\Plugin\Field\FieldWidget;
+use Drupal\Core\Entity\FieldableEntityInterface;
+use Drupal\Core\Field\Plugin\Field\FieldWidget\OptionsButtonsWidget;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Form\OptGroup;
+use Drupal\Core\Form\ConfigFormBase;
+/**
+ * Plugin implementation of the 'salsa_form_options' widget.
+ *
+ * @FieldWidget(
+ *   id = "salsa_form_options",
+ *   module = "bene_salsa",
+ *   label = @Translation("Salsa Form Options"),
+ *   field_types = {
+ *     "string",
+ *   },
+ *   multiple_values = TRUE
+ * )
+ */
+class SalsaFormOptionsWidget extends OptionsButtonsWidget {
+    /**
+     * Returns the array of form options for the widget.
+     *
+     * @param \Drupal\Core\Entity\FieldableEntityInterface $entity
+     *   The entity for which to return options.
+     *
+     * @return array
+     *   The array of options for the widget.
+     */
+    protected function getOptions(FieldableEntityInterface $entity) {
+        if (!isset($this->options)) {
+            $options = \Drupal::service('config.factory')->getEditable('bene_salsa.settings');
+            $this->options = array_flip($options->getRawData());
+        }
+        return $this->options;
+    }
+}
