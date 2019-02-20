06112dbe9c39892313bc9246d5e77c773ef94693
Gabe Carleton-Barnes <gabe@thinkshout.com>
2019-02-20 12:07:28 -0800

    Restore embed divs removed during merge.

--- src/Plugin/Field/FieldFormatter/SalsaEmbedStringFormatter.php
+++ src/Plugin/Field/FieldFormatter/SalsaEmbedStringFormatter.php
@@ -52,9 +52,8 @@ class SalsaEmbedStringFormatter extends StringFormatter {
       '#type' => 'inline_template',
       '#template' => '{{ embed|raw }}',
       '#context' => [
-        'embed' => "<div id='$form_code'><script type='text/javascript' src='https://default.salsalabs.org/api/widget/template/$salsa_code/?tId=$form_code' ></script></div>",
-      ],
-    ];
+        'embed' => "<div id='salsaContainer'><div id='salsaWrapper'><div id='salsaForm'><div id='$form_code'><script type='text/javascript' src='https://default.salsalabs.org/api/widget/template/$salsa_code/?tId=$form_code' ></script></div></div></div></div>",
+      ];
 
     return $elements;
   }
